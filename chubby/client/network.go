ca6774c7e5dfbc8aa070be4f72ceb9163b327877
Sherry Bai <sherrybai01@gmail.com>
2019-05-10 17:41:58 -0400

    switch from host to bridge networking

--- chubby/client/network.go
+++ chubby/client/network.go
@@ -5,9 +5,9 @@ package client
 // We assume that any Chubby node must have one of these addresses.
 // Yes this is gross but we're doing it anyway because of time constraints
 var PossibleServerAddrs = map[string]bool {
-	":5379": true,
-	":6379": true,
-	":7379": true,
-	":8379": true,
-	":9379": true,
+	"172.20.128.1:5379": true,
+	"172.20.128.2:5379": true,
+	"172.20.128.3:5379": true,
+	"172.20.128.4:5379": true,
+	"172.20.128.5:5379": true,
 }
