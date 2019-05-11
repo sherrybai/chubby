87cb9de2ec19730ccde9b4be97f360b556e83b39
Haochen Li <haochenl@princeton.edu>
2019-05-10 20:56:58 -0400

    new test

--- chubby/cmd/testLock_client.go
+++ chubby/cmd/testLock_client.go
@@ -24,10 +24,38 @@ func main() {
 
 	quitCh := make(chan os.Signal, 1)
 	signal.Notify(quitCh, os.Kill, os.Interrupt, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	// Establish two sessions
 	sess1, err := client.InitSession(api.ClientID(clientID1))
 	sess2, err := client.InitSession(api.ClientID(clientID2))
+
+	// Open Locks
 	errOpenLock1 := sess1.OpenLock("LOCK/Lock1")
 	errOpenLock2 := sess2.OpenLock("LOCK/Lock2")
+	isSuccessful, acquireErr := sess1.TryAcquireLock("LOCK/Lock1", api.EXCLUSIVE)
+	if !isSuccessful {
+		log.Printf("Try Acquire Lock failed when it should succeed")
+	}
+	if acquireErr != nil {
+		log.Printf("Try Acquire Lock Unexpected Error")
+		log.Fatal(acquireErr)
+	}
+
+	isSuccessful, acquireErr = sess1.TryAcquireLock("LOCK/Lock1", api.EXCLUSIVE)
+	if isSuccessful {
+		log.Printf("Should fail because the lock we are trying to acquire is in exclusive mode")
+	}
+	if acquireErr == nil {
+		log.Printf("Should fail because the lock we are trying to acquire is in exclusive mode")
+	}
+	isSuccessful, acquireErr = sess1.TryAcquireLock("LOCK/Lock2", api.EXCLUSIVE)
+	if isSuccessful {
+		log.Printf("Should fail because the lock we are trying to acquire a lock that we don't own")
+	}
+	if acquireErr == nil {
+		log.Printf("Should fail because the lock we are trying to acquire a lock that we don't own")
+	}
+
 	if errOpenLock1 != nil {
 		log.Printf("Session 1 has trouble opening lock")
 		log.Fatal(errOpenLock1)
