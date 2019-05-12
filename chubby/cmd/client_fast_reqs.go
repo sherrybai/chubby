b5d6d96d8b36c90284e4d358103806a850b9b66d
Haochen Li <haochenl@princeton.edu>
2019-05-12 00:40:45 -0400

    Number of Operations Per Second

--- chubby/cmd/client_fast_reqs.go
+++ chubby/cmd/client_fast_reqs.go
@@ -21,11 +21,11 @@ func init() {
 }
 
 // Adapted from: https://coderwall.com/p/cp5fya/measuring-execution-time-in-go
-func timeTrack(start time.Time, name string) {
+/*func timeTrack(start time.Time, name string) {
 	elapsed := time.Since(start)
 	fmt.Printf("Start Time is %s\n", start.String())
 	fmt.Printf("%s latency: %s\n", name, elapsed)
-}
+}*/
 
 func main() {
 	// Parse flags from command line.
@@ -49,8 +49,14 @@ func main() {
 
 	fmt.Println("Begin acquiring and releasing locks.")
 
-	var startTime time.Time
-
+	//var startTime time.Time
+	counter := 0
+	go func(count *int) {
+		for  range time.Tick(time.Second) {
+			fmt.Println("We have performed %d operations in the last second", *count)
+			*count = 0
+		}
+	}(&counter)
 	for {
 		select {
 		case <- quitCh:
@@ -58,9 +64,9 @@ func main() {
 			return
 
 		default:
-			startTime = time.Now()
+			//startTime = time.Now()
 			ok, err := sess.TryAcquireLock(lockName, api.SHARED)
-			timeTrack(startTime, "TryAcquire")
+			//timeTrack(startTime, "TryAcquire")
 
 			if err != nil {
 				if sess.IsExpired() {
@@ -72,21 +78,28 @@ func main() {
 					}
 				}
 				log.Printf("TryAcquire failed with error: %s\n", err.Error())
-				continue
+				// Say we acquire a lock, then we pause that node
+				// Release will fail with an error saying connection is closed
+				// When the node is backup before jeopardy is over, the client
+				// would hold the lock, and if we don't comment out continue,
+				// the client will just keep getting the client already owns this lock
+				// error
+				//continue
 			}
 			if !ok {
 				log.Println("Failed to acquire lock. Continuing.")
-				continue
+				//continue
 			}
 
-			startTime = time.Now()
+			//startTime = time.Now()
 			err = sess.ReleaseLock(lockName)
-			timeTrack(startTime, "Release")
+			//timeTrack(startTime, "Release")
 
 			if err != nil {
 				log.Printf("Release failed with error: %s\n", err.Error())
 				continue
 			}
+			counter += 1
 		}
 	}
 }
